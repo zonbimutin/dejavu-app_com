db0a704670cb0f329bd76ef7e51fda5c21aa4630
Felipe Alarcon <zonbimutin@gmail.com>
2022-10-03 14:01:31 +0200

    Update Screen Section

--- sections/Screenshots/index.js
+++ sections/Screenshots/index.js
@@ -1,36 +1,37 @@
+import Image from 'next/image'
 import Slider from 'react-slick'
 import Section from '@components/Basics/Section'
 
 export default function Screenshots() {
   const settings = {
     infinite: true,
-    speed: 500,
-    slidesToShow: 4,
+    speed: 1000,
+    slidesToShow: 5,
     slidesToScroll: 1,
-    centerMode: true,
+    centerMode: false,
     arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
   }
+
+  const images = [1, 3, 2, 5, 6, 7, 8, 9, 0]
+
   return (
-    <Section bg={'bg-backgroundLight'}>
+    <Section alt>
       <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        {images.map((value, key) => {
+          return (
+            <div className="px-8">
+              <Image
+                className="w-5"
+                src={'/img/iphone.png'}
+                width={500}
+                height={1000}
+                layout="intrinsic"
+              />
+            </div>
+          )
+        })}
       </Slider>
     </Section>
   )
